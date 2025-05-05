790aef6cd0f1b8265a0e07e8c4aadb406d2a860d
bkshin <gjrjr4545@gmail.com>
2025-05-05 22:35:39 +0900

    [ubuntu24-rabbitmq] - rabbitmq setup.sh 파일 수정

--- vm/ubuntu24-rabbitmq/scripts/setup.sh
+++ vm/ubuntu24-rabbitmq/scripts/setup.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 set -eux
 
-# 유저 생성
-id bocopile &>/dev/null || sudo useradd -m -s /bin/bash bocopile
+export DEBIAN_FRONTEND=noninteractive
+#!/bin/sh
 
-# 호스트네임 가져오기
-HOSTNAME=$(hostname)
-
-# 디렉토리 생성
-sudo mkdir -p "/log/${HOSTNAME}"
-sudo chown -R bocopile:bocopile /log
-
-# 필수 패키지 설치
-sudo apt-get update
 sudo apt-get install curl gnupg apt-transport-https -y
 
 ## Team RabbitMQ's main signing key
@@ -55,31 +46,15 @@ sudo apt-get install -y erlang-base \
 
 ## Install rabbitmq-server and its dependencies
 sudo apt-get install rabbitmq-server -y --fix-missing
+sudo systemctl restart rabbitmq-server
 
-# 설정 파일 작성
-sudo tee /etc/rabbitmq/rabbitmq.conf > /dev/null <<EOF
-log.dir = /log/${HOSTNAME}/rabbit.log
-EOF
-
-
-# RabbitMQ 활성화 및 시작
-sudo systemctl enable rabbitmq-server
-sudo systemctl start rabbitmq-server
-
-# 서비스 기동 확인 루프 (최대 30초)
-for i in {1..6}; do
-  sudo systemctl is-active rabbitmq-server && break
-  sleep 5
-done
+sleep 10s
 
 # 플러그인 활성화
 sudo rabbitmq-plugins enable rabbitmq_prometheus
 sudo rabbitmq-plugins enable rabbitmq_management
 
-# 계정 생성 및 권한 부여
+# 관리자 계정 생성
 sudo rabbitmqctl add_user admin 12341234
 sudo rabbitmqctl set_user_tags admin administrator
 sudo rabbitmqctl set_permissions -p / admin ".*" ".*" ".*"
-
-# 상태 확인
-sudo rabbitmqctl status || true
